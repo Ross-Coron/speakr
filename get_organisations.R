d50e5f1d31b23b36ba8fe2b812c4f2842ec52896
Ross P. Coron <rpcoron@gmail.com>
2024-11-22 09:27:46 +0000

    Debugged get_org function

--- get_organisations.R
+++ get_organisations.R
@@ -4,14 +4,16 @@ library(tidyverse)
 url <- "https://committees-api.parliament.uk/api/WrittenEvidence?CommitteeBusinessId=1813"
 
 jsonData <- fromJSON(url)
-processedData <- jsonData$items |> select(witnesses)
+jsonData <- jsonData$items |> select(witnesses)
 
 organisations <- c()
-for (i in 1:nrow(processedData)) {
-  #print(x[[i]]$organisations[[1]]$name)
-  if (is.null(x[[i]]$organisations[[1]]$name)) {
+for (i in 1:nrow(jsonData)) {
+  
+  if (is.null(jsonData$witnesses[[i]]$organisations[[1]]$name)) {
     organisations = c(organisations, NA)
   } else {
-  organisations = c(organisations, x[[i]]$organisations[[1]]$name)
+    organisations = c(organisations, jsonData$witnesses[[i]]$organisations[[1]]$name)
   }
-}
+}  
+
+organisations
