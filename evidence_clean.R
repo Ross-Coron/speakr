4f5ac9e41775eb16573dd724d723b8a1ce509155
Ross P. Coron <rpcoron@gmail.com>
2024-11-24 21:26:04 +0000

    Update evidence_clean.R

--- evidence_clean.R
+++ evidence_clean.R
@@ -1,9 +1,5 @@
-# Define REGEX string. Typically meta info from evidence
+# Define REGEX string (most often redundant line at start of evidence)
 regex_string <- "^ISG.*\\(ISG\\s\\d+\\)"
-
-# Looks for string in data frame, removing if found
-evidence_text$extracted_text <- gsub(regex_string, "", evidence_text$extracted_text)
-
 
 removed_text <- c()
 matches <- gregexpr(regex_string, evidence_text$extracted_text)
@@ -11,5 +7,7 @@ extracted <- regmatches(evidence_text$extracted_text, matches)
 for (match in extracted) {
   removed_text <- c(removed_text, match)
 }
-
 writeLines(removed_text, "removed_text.txt")
+
+# Looks for string in data frame, removing if found
+evidence_text$extracted_text <- gsub(regex_string, "", evidence_text$extracted_text)
